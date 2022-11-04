d830e719a0dc66e3e6749b8c0664aa6b0ddeb184
ccbclz <2935230180@qq.com>
2022-11-05 00:27:14 +0800

    第一次提交

---
+++ srb-oss/src/test/java/Test01.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+/**
+ * title:
+ * author:
+ * date:2022/11/5
+ * discription:
+ */
+public class Test01 {
+
+    @Test
+    public void testFor() {
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("荔枝有:" + i + "个");
+        }
+
+    }
+
+}
